61cef0f27dc8604c819a26d789298a7473e82558
Jeff Thomas <jathom30@gmail.com>
2024-01-01 20:09:51 -0500

    added castle

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import './App.css'
-import { Mickey } from './mickey'
+import "./App.css";
+import { Mickey } from "./mickey";
+import castle from "./disneyCastle.jpg";
 
 // a date object for January 26, 2024  7:45 AM EST
-const dDate = new Date(2024, 0, 26, 7, 45, 0, 0)
-const startDate = new Date(2023, 11, 25, 0, 0, 0, 0)
-const today = new Date()
+const dDate = new Date(2024, 0, 26, 7, 45, 0, 0);
+const startDate = new Date(2023, 11, 25, 0, 0, 0, 0);
+const today = new Date();
 
 function App() {
-  const month = dDate.toLocaleString('default', { month: 'long' })
-  const day = dDate.getDate()
-  const year = dDate.getFullYear()
-  const daysSinceStart = Math.floor((today.getTime() - startDate.getTime()) / (1000 * 3600 * 24))
-  const daysUntilDisney = Math.floor((dDate.getTime() - today.getTime()) / (1000 * 3600 * 24)) + 1
-  const percent = Math.floor((daysSinceStart / daysUntilDisney) * 100)
+  const month = dDate.toLocaleString("default", { month: "long" });
+  const day = dDate.getDate();
+  const year = dDate.getFullYear();
+  const daysSinceStart = Math.floor(
+    (today.getTime() - startDate.getTime()) / (1000 * 3600 * 24)
+  );
+  const daysUntilDisney =
+    Math.floor((dDate.getTime() - today.getTime()) / (1000 * 3600 * 24)) + 1;
+  const percent = Math.floor((daysSinceStart / daysUntilDisney) * 100);
 
   return (
-    <div className='App'>
+    <div className="App">
+      <div className="App__castle">
+        <img src={castle} />
+      </div>
       <div className="App__content">
         <p>Disney countdown</p>
-        <div className='App__mickey'>
+        <div className="App__mickey">
           <Mickey fill={percent} />
-          <div className='text-block'>
+          <div className="text-block">
             <h1>{daysUntilDisney}</h1>
             <h2>Days</h2>
           </div>
         </div>
-        <p>{month} {day}, {year}</p>
+        <p>
+          {month} {day}, {year}
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
